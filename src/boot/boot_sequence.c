1b4eb10fe084aba690b3ff2f07c4931368aca3bd
Tauser Carneiro <tauser@gmail.com>
2026-03-31 01:13:06 -0300

    fix(boot): adiciona freertos/task.h para vTaskDelay no teste E29

--- src/boot/boot_sequence.c
+++ src/boot/boot_sequence.c
@@ -27,6 +27,8 @@
 #include "safe_mode_service.h"
 #include "led_router.h"
 
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 #include "esp_log.h"
 #include "esp_err.h"
 #include <inttypes.h>
